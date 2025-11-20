6737eb8f23fd624b36ad8cb300029a8aab816587
Karen Hui Tsao <ktsao@stanford.edu>
2025-11-19 16:16:18 -0800

    CAN2075: Mediathread: Vulnerability Notice: CORS Misconfiguration

--- mediathread/settings_docker.py
+++ mediathread/settings_docker.py
@@ -3,6 +3,11 @@ from mediathread.settings_shared import *
 
 app ="mediathread"
 
+CORS_ALLOWED_ORIGINS = [
+    "https://canvas.stanford.edu",
+    "https://stanford.test.instructure.com"
+]
+
 # required settings:
 SECRET_KEY = os.environ['SECRET_KEY']
 
